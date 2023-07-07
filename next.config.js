1f53f69069eea6e4309f81f53d812e910efbbf74
Sotero29 <soteroarmentagarcia05@gmail.com>
2023-07-07 08:56:24 -0600

    Hola jijijija

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {
+    ...nextConfig,
+    env:{ 
+
+
+
+ZAPATITO_API_KEY: "AIzaSyCUMAeCBU6Cx7Y7hiyWbNG97f4GfHx4VOY",
+ZAPATIRO_AUTH_DOMAIN: "next3a-9d930.firebaseapp.com",
+ZAPATITO_PROJECT_ID: "next3a-9d930",
+ZAPATITO_STORAGE_BUCKET: "next3a-9d930.appspot.com",
+ZAPATITO_MESSAGING_SENDER_ID: "738962696676",
+ZAPATITO_APP_IP: "738962696676",
+},
+
+}
---
+++ src/login/login.js
@@ -0,0 +1,11 @@
+import "./login.css"
+export default function Login(){
+    return(
+        <div className="container">
+            <input type="text" placeholder="email" />
+            <input type="password" placeholder="123456"/>
+            <button>LOGIN</button>
+
+        </div>
+    )
+}
