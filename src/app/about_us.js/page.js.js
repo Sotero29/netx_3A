28cf29e771783db585353e79326378c97ca4ab85
Sotero29 <soteroarmentagarcia05@gmail.com>
2023-08-11 09:07:39 -0600

    Actualización después de hacer pull

--- src/app/about_us.js/page.js.js
+++ src/app/about_us.js/page.js.js
@@ -2,6 +2,8 @@ export default function Home(){
     return(<div>
   
       <h1>titulo</h1>
+      <h1>Sotero 3a</h1>
+      
     </div>
     
     )
---
+++ src/services/firebase/database.js
@@ -0,0 +1,5 @@
+import {getFirestore} from "firebase/getFirestore"
+import  conexion from "./index";
+const db = getFirestore(conexion);
+export default db;d
+
